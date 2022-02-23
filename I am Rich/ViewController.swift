d7d37695b1acce2e840350fbcd59b86050f765c3
baxter1707 <michael_steven.baxter@yahoo.com>
2022-02-23 17:37:11 -0600

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Michael Baxter on 2/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+ 
